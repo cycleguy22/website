b2eff1bc929aea8cbee5e792a48b00ebf8ec0d57
cycleguy22 <104630874+cycleguy22@users.noreply.github.com>
2022-06-22 10:16:42 +0100

    Update gittalk_plugin.js

--- gittalk_plugin.js
+++ gittalk_plugin.js
@@ -1,9 +1,9 @@
 const gitalk = new Gitalk({
-  clientID: 'GitHub Application Client ID',
-  clientSecret: 'GitHub Application Client Secret',
-  repo: 'GitHub repo',      // The repository of store comments,
-  owner: 'GitHub repo owner',
-  admin: ['GitHub repo owner and collaborators, only these guys can initialize github issues'],
+  clientID: '21d1a5313bdc1d36e848',
+  clientSecret: 'a35244a4cf1000c7e8a86ee8b32e7ccce3d87a12',
+  repo: 'website,      // The repository of store comments,
+  owner: 'cycleguy22',
+  admin: ['cycleguy22'],
   id: location.pathname,      // Ensure uniqueness and length less than 50
   distractionFreeMode: false  // Facebook-like distraction free mode
 })
